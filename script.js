299e0ecf79eb93ef1b3eeaf3970265b5271d1e4c
HUY_FA <nguyenvanhuy178tiengiang@gmail.com>
2023-02-23 18:20:22 +0700

    finished main page

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// Script for navigation bar
+
+$(document).ready(function () {
+    // CUSTOM BAR ICON FOR MOBILE SCREEN
+    let bar = $("#bar");
+    let closed = $("#closed");
+    let nav = $('#navbar');
+
+    //SHOW NAVBAR MOBILE
+    if (bar) {
+        bar.click(function () {
+            nav.addClass('active');
+        })
+    }
+
+    //HIDE NAVBAR MOBILE
+    if (closed) {
+        closed.click(function () {
+            nav.removeClass('active')
+        })
+    }
+}); 
